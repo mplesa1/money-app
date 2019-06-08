cee7cafd4c4453924de5a0925773a535e5e36c52
mplesa <mplesa09@gmail.com>
2019-06-08 21:56:44 +0200

    6 lab update

--- src/test/java/hr/java/web/plesa/controller/ExpenseRestControllerTest.java
+++ src/test/java/hr/java/web/plesa/controller/ExpenseRestControllerTest.java
@@ -1,7 +1,58 @@
 package hr.java.web.plesa.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import hr.java.web.plesa.domain.Expense;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
-class ExpenseRestControllerTest {
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ExpenseRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void findAll() throws Exception
+    {
+//        this.mockMvc
+//                .perform(get("/api/expense")
+//                        .with(user("test")
+//                                .password("test")
+//                                .roles("USER", "ADMIN")))
+//                .with(csrf())
+//                .andExpect(status().isOk())
+//                .andExpect(model().attributeExists("expenses"))
+//                .andExpect(model().attribute("expenses", hasSize(1)))
+//                .andExpect(view().name("expenseSearch"));
+
+//        mockMvc.perform(MockMvcRequestBuilders
+//                .get("/api/todo"))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$", hasSize(2)))
+//                .andExpect(jsonPath("$[0].id", is(1)))
+//                .andExpect(jsonPath("$[0].description", is("Lorem ipsum")))
+//                .andExpect(jsonPath("$[0].title", is("Foo")))
+//                .andExpect(jsonPath("$[1].id", is(2)))
+//                .andExpect(jsonPath("$[1].description", is("Lorem ipsum")))
+//                .andExpect(jsonPath("$[1].title", is("Bar")));
+    }
 
 }
